a967bae5d59d5fd5ba792ef35fb926f93a31bdb9
Roberto Marino <rm.marinoroberto@gmail.com>
2025-04-04 17:35:44 +0200

    Finish

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,23 @@
 import React from "react";
 import netflixLogo from "../assets/netflix_logo.png";
 
-const Navbar = () => (
+const Navbar = ({ onNavigate }) => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-black px-3">
     <div className="container-fluid">
       <a className="navbar-brand" href="#">
         <img src={netflixLogo} alt="Netflix Logo" height="40" />
       </a>
-
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarNav">
+      <div className="collapse navbar-collapse">
         <ul className="navbar-nav ms-3">
-          <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
           <li className="nav-item"><a className="nav-link" href="#">TV Shows</a></li>
           <li className="nav-item"><a className="nav-link" href="#">Movies</a></li>
           <li className="nav-item"><a className="nav-link" href="#">Recently Added</a></li>
           <li className="nav-item"><a className="nav-link" href="#">My List</a></li>
         </ul>
-
-        <div className="ms-auto d-flex align-items-center">
-          <a href="#" className="text-white me-3"><i className="bi bi-search"></i></a>
-          <a href="#" className="text-white me-3 fw-bold">KIDS</a>
-          <a href="#" className="text-white me-3"><i className="bi bi-bell"></i></a>
-
-          <div className="dropdown">
-            <a href="#" className="text-white dropdown-toggle d-flex align-items-center" id="profileDropdown" data-bs-toggle="dropdown" role="button">
-              <i className="bi bi-person-circle fs-4 me-1"></i>
-            </a>
-            <ul className="dropdown-menu dropdown-menu-dark dropdown-menu-end" aria-labelledby="profileDropdown">
-              <li><a className="dropdown-item" href="#">Account</a></li>
-              <li><a className="dropdown-item" href="#">Settings</a></li>
-              <li><a className="dropdown-item" href="#">Logout</a></li>
-            </ul>
-          </div>
+        <div className="ms-auto d-flex align-items-center gap-3">
+          <button className="btn btn-link text-white text-decoration-none" onClick={() => onNavigate("home")}>Home</button>
+          {/* <button className="btn btn-link text-white text-decoration-none" onClick={() => onNavigate("settings")}>Settings</button> */}
+          {/* <button className="btn btn-link text-white text-decoration-none" onClick={() => onNavigate("profile")}>Profile</button> */}
         </div>
       </div>
     </div>
